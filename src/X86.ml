794d9835c8456da297a181a9b2cb606bacb7f989
sandwwraith <sandwwraith@gmail.com>
2019-04-14 19:42:41 +0300

    X86 for hw7

--- src/X86.ml
+++ src/X86.ml
@@ -83,20 +83,13 @@ let show instr =
 (* Opening stack machine to use instructions without fully qualified names *)
 open SM
 
-(* Symbolic stack machine evaluator
+let rec range l h = if l >= h then [] else l :: (range (l + 1) h)
 
-     compile : env -> prg -> env * instr list
-
-   Take an environment, a stack machine program, and returns a pair --- the updated environment and the list
-   of x86 instructions
-*)
-let compile env code = failwith "Not implemented"
-                                
 (* A set of strings *)           
 module S = Set.Make (String)
 
 (* Environment implementation *)
-let make_assoc l = List.combine l (List.init (List.length l) (fun x -> x))
+let make_assoc l = List.combine l (range 0 (List.length l))
                      
 class env =
   object (self)
@@ -160,6 +153,95 @@ class env =
       List.filter (function R _ -> true | _ -> false) stack
       
   end
+
+(* Symbolic stack machine evaluator
+
+     compile : env -> prg -> env * instr list
+
+   Take an environment, a stack machine program, and returns a pair --- the updated environment and the list
+   of x86 instructions
+*)
+let rec compile (env:env) program = List.fold_left compile1 (env, []) program
+  and compile1 (env, prg) ins =
+  let ref_global (e:env) x = let e' = e#global x in e'#loc x, e' in
+  let newenv, newprg = match ins with
+    | LABEL l -> env, [Label l]
+    | JMP l -> env, [Jmp l]
+    | CJMP (c, l) -> 
+      let loc, env = env#pop in
+      env, [Binop ("cmp", L 0, loc); CJmp (c, l)]
+    | CONST n ->
+      let loc, env = env#allocate in
+      env, [Mov (L n, loc)]
+    | WRITE ->
+      let loc, env = env#pop in
+      env, [Push loc; Call "Lwrite"; Pop eax]
+    | READ ->
+      let loc, env = env#allocate in
+      env, [Call "Lread"; Mov(eax, loc)]
+    | LD x ->
+      let ref, env = ref_global env x in
+      let loc, env = env#allocate in
+      env, [Mov (ref, eax); Mov (eax, loc)]
+    | ST x ->
+      let ref, env = ref_global env x in
+      let loc, env = env#pop in
+      env, [Mov (loc, eax); Mov(eax, ref)]
+    | CALL (f, arg_cnt, has_ret) -> 
+      let accumulate = (fun (env, operands) _ -> let operand, env = env#pop in (env, operand::operands)) in
+      let env, args = List.fold_left accumulate (env, []) (range 0 arg_cnt) in
+      let pushed = List.map (fun x -> Push x) args in
+      let env, res_code = if has_ret then let (loc, env') = env#allocate in env', [Mov (eax, loc)] else env, [] in
+      env, pushed @ [Call f; Binop ("+", (L (arg_cnt * word_size)), esp)] @ res_code
+    | BEGIN (f, args, locals) -> 
+      let env = env#enter f args locals in
+      let pushed = List.map (fun x -> Push (R x)) (range 0 num_of_regs) in
+      env, [Push ebp; Mov (esp, ebp)] @ pushed @ [Binop ("-", M ("$" ^ env#lsize), esp)]
+    | END -> 
+      let epilogue = [Mov (ebp, esp); Pop ebp; Ret; Meta (Printf.sprintf "\t.set %s, %d" env#lsize (env#allocated * word_size))] in
+      let poped = List.map (fun x -> Pop (R x)) (List.rev (range 0 num_of_regs)) in
+      env, [Label env#epilogue] @ poped @ epilogue
+    | RET has_ret -> if has_ret
+        then let ret, env = env#pop in env, [Mov (ret, eax); Jmp env#epilogue]
+        else env, [Jmp env#epilogue]
+    | BINOP op -> 
+      let op2, op1, env = env#pop2 in
+      let result, env = env#allocate in
+      let simple_op cmd = match (op1, op2) with 
+        | (S _, S _) -> [Mov (op1, eax); Binop (cmd, op2, eax); Mov(eax, result)]
+        | _ -> if result = op1 then [Binop (cmd, op2, op1)] else [Binop (cmd, op2, op1); Mov (op1, result)] 
+      in
+      let div_op cmd = let reg = if cmd = "/" then eax else edx in
+       [Mov(op1, eax); Binop("^", edx, edx); Cltd; IDiv op2; Mov(reg, result)] 
+      in
+      let cmp_op cmd = let suffix = match cmd with 
+        | ">" -> "g"
+        | ">=" -> "ge"
+        | "<" -> "l"
+        | "<=" -> "le"
+        | "==" -> "e"
+        | "!=" -> "ne"
+        | _ -> failwith "Unknown comparison operator"
+      in match (op1, op2) with 
+        | (S _, S _) -> [Binop("^", eax, eax); Mov (op1, edx); Binop ("cmp", op2, edx); Set(suffix, "%al"); Mov(eax, result)]
+        | _ -> [Binop("^", eax, eax); Binop ("cmp", op2, op1); Set(suffix, "%al"); Mov(eax, result)]
+      in
+      let insn = match op with 
+        | "+" | "-" | "*" -> simple_op op
+        | "/" | "%" -> div_op op
+        | ">" | ">=" | "<" | "<=" | "==" | "!=" -> cmp_op op
+        | "!!" -> [Binop("^", eax, eax); Mov (op1, edx); Binop ("!!", op2, edx); Set("nz", "%al"); Mov(eax, result)]
+        (* imul does not set ZF :( *)
+        | "&&" -> [
+          Binop("^", eax, eax); Binop("^", edx, edx); 
+          Binop("cmp", L 0, op1); Set("ne", "%al"); 
+          Binop("cmp", L 0, op2); Set("ne", "%dl");
+          Binop("&&", edx, eax); Mov(eax, result)]
+        | _ -> failwith ("Unknown operator " ^ op)
+      in env, insn
+  in (newenv, prg @ newprg)
+
+
   
 (* Generates an assembler text for a program: first compiles the program into
    the stack code, then generates x86 assember code, then prints the assembler file
